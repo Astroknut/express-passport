f31f5547417a8a06c1b8e6bb073c97995aa35de7
Astroknut <charlesbarrett1521@gmail.com>
2017-08-01 14:04:16 -0600

    adds dependencies

---
+++ starter-code/config/passport.js
@@ -0,0 +1,33 @@
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+module.exports = function(passport){
+	passport.use('local-signup', new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password',
+		passReqToCallback: true
+	}, function(req,email,password,callback){
+		//Find a user with a given email
+		User.findOne({'local.email': email}, function(err,user){
+			if (err) return callback(err);
+
+			//If user already exists
+			if(user){
+				return callback(null,false,req.flash('signupMessage', "This email is already taken!"));
+			} else {
+				//User does not exist
+				//Create user
+				let newUser = new User();
+				newUser.local.email = email;
+				newUser.local.passwoprd = newUser.hash(password);
+
+				newUser.save(function(err){
+					if(err) throw err;
+					return callback(null,newUser);
+				});
+			}
+		});
+	}));
+
+};
